00d42a50eae18899707dd681b9f22f8bf83a98c6
Admin <15655111597@163.com>
2019-07-11 21:23:03 +0800

    test

---
+++ src/test/java/com/yxd/adios/AdiosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yxd.adios;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdiosApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
